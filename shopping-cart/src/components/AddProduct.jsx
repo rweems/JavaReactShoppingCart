3117dcebfacf928c4d1a7233cc514df5ce4fc0a5
Rickey <weems505@gmail.com>
2020-01-13 13:51:02 -0500

    React Components

---
+++ shopping-cart/src/components/AddProduct.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class AddProduct extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <h1>Add Product Component</h1>
+         );
+    }
+}
+ 
+export default AddProduct;
