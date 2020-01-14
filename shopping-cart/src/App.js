269b8c4a79874e3c7eb5de9a617267caf2001352
Mohamedzaeem <mohamedmomin5994@gmail.com>
2020-01-14 15:43:12 -0500

    product

--- shopping-cart/src/App.js
+++ shopping-cart/src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
+
 import './App.css';
 import Products from "./components/Products";
 import Cart from "./components/Cart";
--- shopping-cart/src/components/Products.jsx
+++ shopping-cart/src/components/Products.jsx
@@ -2,30 +2,52 @@ import React, {Component} from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faShoppingCart} from '@fortawesome/free-solid-svg-icons';
 import '../css/Products.css';
+import axios from 'axios';
+import {Container,Row,Col}from 'react-bootstrap';
+
 
 
 class Products extends Component{
-    state = {}
+    constructor(){
+        super();
+        this.display();
+    }
+    state = {
+        prodcuts:[]
+    }
+
+    display = () => {
+        axios.get(`http://localhost:8080/products`).then(res => {
+        const productlist = res.data;
+        this.setState({ prodcuts:productlist });
+      })
+    }
+    addtocart = (id) =>{
+        console.log(id);
+    }
+
     render() {
-        return (
-
-            <div class="container">
+        const productList = this.state.prodcuts.map(function(product){
+            return (<Col sm={4}> <div class="card" style={{width:'20rem',marginTop:'30px'}}>
+            <img src={`data:image/png;base64,${product.image}`} style={{height:'20rem'}}class="card-img-top" />
+            <div class="card-body">
                 
-                <div class="card" style={{width:'20rem'}}>
-                    <img src={require('../img/mac.jpg')} class="card-img-top" />
-                    <div class="card-body">
-                        
-                        <p class="card-text">Designed for those who defy limits and change the world, the new MacBook Pro 
-                        is by far the most powerful notebook we’ve ever made. </p>
-                        <h5 class="card-title">Macbook Pro<span>$1000</span></h5>
-                        <div className="cardstuff">
-                            Qty:<input type="number" style={{width:"50px"}} min="1" maxlength="3" name="qty"/> 
-                            <button><FontAwesomeIcon icon={faShoppingCart} /></button>
-                        </div>
-                    </div>
+                <p class="card-text"style={{width:'20rem'}}>{product.description.length} </p>
+                <h5 class="card-title">{product.name}<span>{product.price}</span></h5>
+                <div className="cardstuff">
+                    Qty:<input type="number" style={{width:"50px"}} min="1" maxlength="3" name="qty"/> 
+                    <button onClick={this.addtocart.bind(this,1)}><FontAwesomeIcon icon={faShoppingCart} /></button>
                 </div>
-
             </div>
+        </div></Col>);
+          });
+        return (
+            
+            <Container>
+                <Row>
+                    {productList}
+                </Row>
+            </Container>
 
 
         );
