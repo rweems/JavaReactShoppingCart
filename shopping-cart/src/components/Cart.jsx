91df01047732c5f7cd3b243c9b06e4d29661371f
Mohamedzaeem <mohamedmomin5994@gmail.com>
2020-01-16 09:57:24 -0500

    cart update

--- shopping-cart/src/components/Cart.jsx
+++ shopping-cart/src/components/Cart.jsx
@@ -1,10 +1,63 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import {Table,Container}from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faTrashAlt} from '@fortawesome/free-solid-svg-icons';
+
+
 
 class Cart extends Component {
-    state = {  }
+    constructor(){
+        super();
+        this.getcart();
+        this.updatetotal();
+    }
+    state = { orders:[], carttotal:0 }
+
+    
+    getcart=() =>{
+        axios.get(`http://localhost:8080/orders`).then(res => {
+            const orderlist = res.data;
+            this.setState({ orders:orderlist });
+            console.log(this.state.orders);
+          })
+    }
+
+    delete=(id)=>{
+        axios.delete(`http://localhost:8080/orders/`+id).then((res)=>{
+            this.getcart();
+        });
+    }
+    updatetotal=()=>{
+      for (const [index, value] of this.state.orders.entries()) {
+        this.state.carttotal+=value;
+      }
+    }
     render() { 
         return (
-            <h1>Cart Component</h1>
+            <Container>
+            <Table responsive borderless className="text-center">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Price</th>
+                <th>Quantity</th>
+                <th>total</th>
+              </tr>
+            </thead>
+            <tbody  >
+            {this.state.orders.map((order) =>(<tr>
+                <td>{order.prod.name}</td>
+                <td>{order.prod.price}</td>
+                <td>{order.quantity}
+                <FontAwesomeIcon icon={faTrashAlt} onClick={(e) => this.delete(order.id)} className="float-right pr-auto mt-1"/>
+                </td>
+                <td>{order.quantity * order.prod.price}</td>
+              </tr>))}
+            </tbody>
+          </Table>
+    <h1>Total: {this.state.carttotal}</h1>
+          </Container>
           );
     }
 }
--- src/main/java/com/pyramid/shoppingcart/controller/OrderController.java
+++ src/main/java/com/pyramid/shoppingcart/controller/OrderController.java
@@ -1,10 +1,13 @@
 package com.pyramid.shoppingcart.controller;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -42,10 +45,15 @@ public class OrderController {
 	}
 	
 	@GetMapping
-	public String hello() {
-		return "Hello";
+	public List<order> getcart(){
+		return os.getAllOrders();
 	}
 	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+		os.deleteOrder(id);
+	}
+
 
 	
 }
